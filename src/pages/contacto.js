84a2e07e5d25d35ef86f2e6da944edf00ef8190c
Alan Marcelo Flores <69022647+AlanMFlores@users.noreply.github.com>
2023-09-21 16:12:12 -0300

    textos justificados

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -14,15 +14,15 @@ const Contact = () => {
         <section className='mx-8 md:mx-16 lg:mx-32 pt-5 lg:pt-20'>
         <h2 className='text-2xl font-semibold mb-6'>Contacto</h2>
         <div className='flex flex-col gap-6 mb-16'>
-            <p className='font-light leading-8'>
+            <p className='font-light leading-8 text-justify'>
             Nuestra productora de eventos es una empresa dedicada a la <span className='text-mainColor'>planificación, organización y ejecución de todo tipo de eventos</span>. Nos especializamos en crear experiencias únicas y memorables, diseñadas a medida para satisfacer las necesidades y deseos de nuestros clientes.</p>
-            <p className='font-light leading-8'>
+            <p className='font-light leading-8 text-justify'>
             Nuestro equipo de <span className='text-mainColor'>profesionales altamente capacitados y apasionados por su trabajo</span> se encarga de cada detalle, desde la conceptualización hasta la implementación, asegurando que cada evento sea un éxito rotundo. Trabajamos estrechamente con nuestros clientes para entender su visión, objetivos y presupuesto, y luego transformamos esas ideas en realidad.</p>
-            <p className='font-light leading-8'>
+            <p className='font-light leading-8 text-justify'>
             Ya sea que estemos organizando una conferencia empresarial, una gala benéfica, una boda de ensueño o un concierto multitudinario, nos esforzamos por brindar servicios de primera calidad. Coordinamos todos los aspectos logísticos, como la selección de lugares, la gestión de proveedores, la decoración, la iluminación, el sonido, el catering y mucho más. Nos aseguramos de que todo funcione sin problemas para que <span className='text-mainColor'>nuestros clientes y sus invitados disfruten al máximo del evento</span>.</p>
-            <p className='font-light leading-8'>
+            <p className='font-light leading-8 text-justify'>
             Además de la planificación y ejecución, también ofrecemos servicios de <span className='text-mainColor'>asesoramiento y consultoría en eventos</span>. Compartimos nuestra experiencia y conocimientos para ayudar a nuestros clientes a tomar decisiones informadas y estratégicas en cuanto a la temática, el formato y la promoción de sus eventos.</p>
-            <p className='font-light leading-8'>
+            <p className='font-light leading-8 text-justify'>
             En nuestra productora de eventos, nos enorgullece ofrecer un servicio personalizado, una atención meticulosa a los detalles y una pasión inquebrantable por crear experiencias inolvidables. Nuestro objetivo es <span className='text-mainColor'>superar las expectativas en cada evento y dejar una impresión duradera en todos los asistentes</span>.</p>
         </div>
 
